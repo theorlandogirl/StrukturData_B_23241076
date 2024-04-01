7a97f8ab5f5e5639d1060a9c3e8bc1727c27c8e8
theorlandogirl <146050758+theorlandogirl@users.noreply.github.com>
2024-04-01 12:47:29 +0800

    pertemuan kedua

---
+++ meet2.py.py
@@ -0,0 +1,21 @@
+# Belajar Variabel
+# assigment
+x = 3
+y = 6
+uang = 1000
+
+# pemanggilan
+print ("Nilai Uas saya =", x)
+print ("Nilai Uts saya =", y)
+print ("sisa uang saya =", uang)
+
+#penamaaan variabel
+nilai_a = 9 # tidak boleh spasi
+kotak10 = 10 # tidak boleh diawali angka
+
+#input variabel kedua
+x = 30
+print ("Nilai Uas saya =", x)
+x = 300
+print ("Nilai Uas saya =", x)
+
