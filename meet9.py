81181e0ec663f291f7ca999d7dd1e51cb952fd25
theorlandogirl <146050758+theorlandogirl@users.noreply.github.com>
2024-06-03 12:38:17 +0800

    03/06/2024

---
+++ meet9.py
@@ -0,0 +1,20 @@
+# Percabangan (Jika, Maka)
+
+nama = input("What is your name: ")
+
+#if kondisi: aksi
+if nama == "Edy" : print("Dosen Ganteng")
+print("End of Program")
+
+# If indentasi
+if nama == "Edy" :
+    print("Dosen Ganteng")
+    print("Ganteng banget")
+print("end of program")
+
+# IF ELSE
+if nama == "Edy" :
+    print("Dosen Ganteng")
+else:
+    print("Anda tidak Ganteng")
+print("end of program")
